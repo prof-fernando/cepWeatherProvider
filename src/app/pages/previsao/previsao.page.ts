50390f36f9f7d6f1bcef8571b5ed4de285ea521e
Fernando luis <fernando.tetu@gmail.com>
2019-09-09 21:36:26 -0300

    adicionando controle de linguage e apresentando dados na tela

--- src/app/pages/previsao/previsao.page.ts
+++ src/app/pages/previsao/previsao.page.ts
@@ -8,10 +8,16 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./previsao.page.scss']
 })
 export class PrevisaoPage implements OnInit {
+  resposta: any;
+  icone: string;
   constructor(private tempo: PrevisaoService, private rota: ActivatedRoute) {
     const cidade = rota.snapshot.params['cidade'];
     console.log(cidade);
-    tempo.buscarPrevisao(cidade).subscribe(resp => console.log(resp));
+    tempo.buscarPrevisao(cidade).subscribe(resp => {
+      this.resposta = resp;
+      const codigo = resp.weather[0].icon;
+      this.icone = `http://openweathermap.org/img/wn/${codigo}@2x.png`;
+    });
   }
 
   ngOnInit() {}
--- src/app/services/previsao.service.ts
+++ src/app/services/previsao.service.ts
@@ -9,7 +9,11 @@ export class PrevisaoService {
   constructor(private http: HttpClient) {}
 
   public buscarPrevisao(cidade: string): Observable<any> {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade},br&appid=37e4defe733067fef8b3c3ec85ce8e3c`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade},br&appid=37e4defe733067fef8b3c3ec85ce8e3c`;
+    // altera o padrao celcius
+    url += '&units=metric';
+    // define a idioma
+    url += '&lang=pt';
     return this.http.get(url);
   }
 }
